9f12d31b2428e9e55b0c9dfb41434cb892dc31de
sabrinacouto <sabrinacoutolima@gmail.com>
2023-11-28 18:16:02 -0300

    inserindo links

--- main.js
+++ main.js
@@ -21,11 +21,27 @@ btnMobile.addEventListener('touchstart', toggleMenu);
 function showAdditionalText() {
   const additionalText = document.getElementById("additionalText");
 
+  // Verifica se o texto está oculto
   if (additionalText.classList.contains("hidden")) {
     additionalText.style.display = "block";
+    //Exibe o texto
     additionalText.classList.remove("hidden");
   } else {
+     // Oculta o texto
     additionalText.style.display = "none";
     additionalText.classList.add("hidden");
   }
 }
+
+// Integrando o bot com o html
+window.watsonAssistantChatOptions = {
+  integrationID: "4855c890-ba07-45ce-a884-fffb3f8272f8", // The ID of this integration.
+  region: "us-south", // The region your integration is hosted in.
+  serviceInstanceID: "824383ff-fd9b-4ac1-9d86-3ec21aa89c21", // The ID of your service instance.
+  onLoad: async (instance) => { await instance.render(); }
+};
+setTimeout(function(){
+  const t=document.createElement('script');
+  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+  document.head.appendChild(t);
+});
